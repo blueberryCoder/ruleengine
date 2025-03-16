74e9050d12c4f6e419fdfd86ea3d84ca01f24183
muyonggang <mu.yonggang@kamivison.com>
2025-03-16 18:31:38 +0800

    refactor: 重构路由

--- src/router/index.ts
+++ src/router/index.ts
@@ -3,45 +3,68 @@ import HomeView from '../views/HomeView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
+
+  
   routes: [
+    
+    {
+      path: '/:catchAll(.*)',
+      name: '404',
+      component:() => import('../pages/exception/404.vue')
+    },
+    {
+      path: '/login',
+      name: 'login',
+      component: () => import('../pages/login/Login.vue')
+    },
+
     {
       path: '/',
-      name: 'home',
-      component: HomeView,
+      name: '首页',
+      redirect: '/dashboard',
+      component: () => import('../layouts/tabs/TabsView.vue'),
+      children: [
+        {
+          path: 'dashboard',
+          name: 'dashboard',
+          component: HomeView,
+        },
+        {
+          path: '/rules',
+          name: 'rules',
+          component: () => import('../views/RulesView.vue'),
+        },
+        {
+          path: '/test',
+          name: 'test',
+          component: () => import('../views/TestView.vue'),
+        },
+        {
+          path: '/stats',
+          name: 'stats',
+          component: () => import('../views/StatsView.vue'),
+        },
+        {
+          path: '/connections',
+          name: 'connections',
+          component: () => import('../views/ConnectionsView.vue'),
+        },
+        {
+          path: '/settings',
+          name: 'settings',
+          component: () => import('../views/SettingsView.vue'),
+        },
+        {
+          path: '/about',
+          name: 'about',
+          // route level code-splitting
+          // this generates a separate chunk (About.[hash].js) for this route
+          // which is lazy-loaded when the route is visited.
+          component: () => import('../views/AboutView.vue'),
+        },
+      ]
     },
-    {
-      path: '/rules',
-      name: 'rules',
-      component: () => import('../views/RulesView.vue'),
-    },
-    {
-      path: '/test',
-      name: 'test',
-      component: () => import('../views/TestView.vue'),
-    },
-    {
-      path: '/stats',
-      name: 'stats',
-      component: () => import('../views/StatsView.vue'),
-    },
-    {
-      path: '/connections',
-      name: 'connections',
-      component: () => import('../views/ConnectionsView.vue'),
-    },
-    {
-      path: '/settings',
-      name: 'settings',
-      component: () => import('../views/SettingsView.vue'),
-    },
-    {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue'),
-    },
+
   ],
 })
 
